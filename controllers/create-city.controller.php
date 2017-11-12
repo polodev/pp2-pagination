b3ab13df7c0da25dfe0cc6a66b551c1d6bd3cfce
Polo Dev <polodev10@gmail.com>
2017-11-13 02:17:00 +0600

    update readme.md

--- controllers/create-city.controller.php
+++ controllers/create-city.controller.php
@@ -1,7 +1,6 @@
 <?php
 Use Illuminate\Database\Capsule\Manager;
 if (isset($_POST['city']) && isset($_POST['country'])) {
-  echo 'hello world';
   $city = $_POST['city'];
   $country = $_POST['country'];
   City::create([
